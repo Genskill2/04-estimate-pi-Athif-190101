238543500408e3181d3e3be95c978753b2f7bc0f
Athif-190101 <85243603+Athif-190101@users.noreply.github.com>
2021-06-04 17:21:22 +0400

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,22 +5,20 @@
 
 float wallis_pi(int);
 
-int main(void) {
-  float pi;
-  for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
-    }
-  }
-
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      abort();
-    }
-  }
+int main(void) 
+{int n;
+ float pi;
+ printf("enter the number of iterations:");
+ scanf("%d",n);
+ pi=wallis_pi(n);
+ printf("the approximate value of pi is:%f",pi);
+ return 0;
 }
 
+float wallis_pi(int n1)
+{float pi,wi;
+  for(i=1;i<=n1;i++)
+   wi+=(4*(i*i))/(4*(i*i)-1);
+ pi=2*wi;
+ return pi;
+}
